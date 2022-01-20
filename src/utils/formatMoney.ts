b05c0608f67902bad8b195a10659d742ec338d7b
tedcurrent <tetsiiedm@gmail.com>
2022-01-20 23:29:39 +0200

    Some initial charting

--- src/utils/formatMoney.ts
+++ src/utils/formatMoney.ts
@@ -3,12 +3,13 @@ export type Money = {
   value: number
 }
 
-export const formatMoney = (money: Money) => {
+export const formatMoney = (money: Money, opts?: Intl.NumberFormatOptions) => {
   const formatter = Intl.NumberFormat('fi-FI', {
     style: 'currency',
     currency: money.currency,
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2
+    maximumFractionDigits: 2,
+    ...opts
   })
 
   return formatter.format(money.value)
