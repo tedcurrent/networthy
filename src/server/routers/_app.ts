663f2d5e28dc35ae414070fcfe447b8291da4c9f
tedcurrent <tetsiiedm@gmail.com>
2022-01-30 15:00:46 +0200

    Add getAll route for BalanceType

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,9 +1,11 @@
 import { createRouter } from '../createRouter'
+import { balanceTypeRouter } from './balanceType'
 import { networthRouter } from './networth'
 import { trendRouter } from './trend'
 
 export const appRouter = createRouter()
   .merge('networth.', networthRouter)
   .merge('trend.', trendRouter)
+  .merge('balanceType.', balanceTypeRouter)
 
 export type AppRouter = typeof appRouter
